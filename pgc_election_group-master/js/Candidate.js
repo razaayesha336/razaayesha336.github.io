af596228878792ad8c923bb24f30fecc8e78314c
Ayesha Raza <razaayesha336@gmail.com>
2019-11-01 22:57:52 -0400

    Proj

---
+++ pgc_election_group-master/js/Candidate.js
@@ -0,0 +1,45 @@
+function Candidate(config){
+
+	var self = this;
+	Draggable.call(self, config);
+
+	// Passed properties
+	self.id = config.id;
+	self.size = 40;
+
+	// GRAPHICS
+	var _graphics = Candidate.graphics[self.id];
+	self.fill = _graphics.fill;
+	self.img = new Image();
+	self.img.src = _graphics.img;
+
+	self.draw = function(ctx){
+
+		// RETINA
+		var x = self.x*2;
+		var y = self.y*2;
+		var size = self.size*2;
+
+		// Draw image instead!
+		ctx.drawImage(self.img, x-size/2, y-size/2, size, size);
+
+	};
+
+}
+
+// CONSTANTS: the GRAPHICS!
+// id => img & fill
+Candidate.graphics = {
+	square: {
+		img: "img/square.png",
+		fill: "hsl(240,80%,70%)"
+	},
+	triangle: {
+		img: "img/triangle.png",
+		fill: "hsl(45,80%,70%)"
+	},
+	hexagon: {
+		img: "img/hexagon.png",
+		fill: "hsl(0,80%,70%)"
+	}
+};
